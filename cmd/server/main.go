fce7a7711f7c23ef5e46249f4199d0714036e947
cuji <alexmetal66@gmail.com>
2025-12-19 20:03:45 -0500

    feat: add base dependency for http routing properly

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+)
+
 // start point baby
 func main() {
+	l := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello Ownned"))
+		if err != nil {
+			l.Warn("some error happend", "err", err)
+		}
+	})
+
+	PORT := 9090
+	l.Info("server starting at:", "port", PORT)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), r); err != nil && err != http.ErrServerClosed {
+		l.Error("server could not start", "err", err)
+	}
 
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.2
 require (
 	github.com/go-playground/validator/v10 v10.28.0
 	github.com/google/uuid v1.6.0
+	github.com/go-chi/chi/v5 v5.2.3
 )
 
 require (
